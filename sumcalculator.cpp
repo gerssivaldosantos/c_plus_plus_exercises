dde20aa1f9808997c297b6fcd73e70bff68d4f75
andows159 <canalandows@gmail.com>
2021-02-22 19:45:20 -0300

    Sum calculator

---
+++ sumcalculator.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    //variable declaration
+    int num1, num2; 
+    int sum;
+    //input message
+    cout << "Type a number ";
+    //input save on variable
+    cin >> num1;
+    cout << "Type a number ";
+    cin >> num2;
+    sum = num1 + num2;
+    //print sum
+    cout << "Sum is:" << sum;
+
+}
