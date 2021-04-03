66f9440c0596f9de602a5248161ba40cab0a8eee
andows159 <canalandows@gmail.com>
2021-04-03 07:02:23 -0300

    isprim

--- aulas/isprim.c
+++ aulas/isprim.c
@@ -2,21 +2,8 @@
 int main()
 {
     int number, cont;
-    
-    printf("Type a number");
-    scanf("%d", &number);
-
+    printf("Type a number : ");scanf("%d", &number);
     for (int i = 2; i < number; i++)
-    {
-        printf(" resto %d de %d percent %d  \n", number % i, number, i);
-        if (number % i == 0)
-        {
-            printf("%d is not a prim \n", number);
-            break;
-        }
-        else
-        {
-            printf("%d testado\n", i);
-        }
-    }
+    {if (number % i == 0){printf("not ");break;} }
+    printf("is prim\n");
 }
--- college flux control/003.c
+++ college flux control/003.c
@@ -1,21 +1,9 @@
 #include <stdio.h>
-
 int main()
 {
-    int number;
-    printf("type a number");
-    scanf("%d", &number);
+    int number, cont;
+    printf("Type a number : ");scanf("%d", &number);
     for (int i = 2; i < number; i++)
-    {
-        if (number % i == 0)
-        {
-            printf("%d is not a prim \n", number);
-            break;
-        }
-        else if (number % i == 1)
-        {
-            printf("%d is a prim \n", number);
-            break;
-        }
-    }
+    {if (number % i == 0){printf("not ");break;} }
+    printf("is prim\n");
 }
