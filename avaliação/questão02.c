ec106659f22ce48531dd64d58ff6d474ea77680d
andows159 <canalandows@gmail.com>
2021-04-24 07:31:14 -0300

    desisto

--- avaliação/questão02.c
+++ avaliação/questão02.c
@@ -2,20 +2,14 @@
 
 int main() {
   int price, coin1, coin2, coin3;
-  int globalAux, biggest, second, third;
+  int globalCoinCont = 0, globalAux, biggest, second, third;
   int biggestCont = 0, secondCont = 0, thirdCont = 0;
   printf("Type the price of product: ");
   scanf("%d", &price);
-  for (int i = 1; i <= 3; i++) {
-    printf("type the %dº coin value :", i);
-    scanf("%d", &globalAux);
-    if (i == 1)
-      coin1 = globalAux;
-    else if (i == 2)
-      coin2 = globalAux;
-    else
-      coin3 = globalAux;
-  }
+  printf("Type the coins values (not is be need that the coins will are in order)\n\n ");
+  printf("Type de first coin value : ");scanf("%d",&coin1);
+  printf("Type de second coin value : ");scanf("%d",&coin2);
+  printf("Type de third coin value : ");scanf("%d",&coin3);
   if (coin1 > coin2 && coin1 > coin3) {
     biggest = coin1;
     if (coin2 > coin3) {
@@ -87,6 +81,8 @@ int main() {
          coin3);
   printf("the sequence of big to small\n1º:%d\n2º%d\n3º%d\n  \n", biggest,
          second, third);
+  
+
   if ((price / biggest) > 0) {
     biggestCont = price / biggest;
     price = price % biggest;
@@ -117,7 +113,14 @@ int main() {
     printf("foram usada %d moedas pequenas, Sobrou %d \n", thirdCont, price);
   }
   if (price > 0) {
-    printf(" restante final do preço %d IMPOSSIVEL\n", price);
+    printf("Impossivel (não tanto) \n\n\n");
+
+    /* price += (biggestCont*biggest) + (secondCont*second) + (thirdCont*third);
+    printf("%d",price/second); */
+    
+
+    //colocar aqui a mesma verificação que fiz em ordem decrescente, mas em outras
+    //ordens para tentar solucionar o problema do 1000, 150, 35 , 27
   }
 }
 
@@ -139,9 +142,32 @@ foram usada 2 moedas médias, preço atual 30
 foram usada 1 moedas pequenas, Sobrou 3 
  restante final do preço 3 IMPOSSIVEL
 
+ tem que dar 2 moedas grandes(150) e 20 médias (35)
+
  */
+/* 
+Type the price of product: 500
+Type the coins values (not is be need that the coins will are in order)
 
+ Type de first coin value : 127
+Type de second coin value : 100
+Type de third coin value : 51
 
+first coin 127, second coin 100, third coin 51
+  
+the sequence of big to small
+1º:127
+2º100
+3º51
+  
+foram usadas 3 moedas grandes, preço atual 119
+foram usada 0 moedas médias, preço atual 119 
+foram usada 2 moedas pequenas, Sobrou 17 
+Impossivel (não tanto) 
+
+era pra dar 5 moedas médias
+
+ */
 
 /* Escreva um programa em C, que dado o preço de uma mercadoria e os valores das
  três moedas disponíveis, calcule o menor número possível de moedas necessário
